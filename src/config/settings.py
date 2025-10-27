4f251c0aca6dfbc854fa7eacfa91e42df3469cf8
Stoney Jackson <dr.stoney@gmail.com>
2025-10-27 15:16:51 +0000

    refactor: remove legacy src/config/settings.py

--- src/config/settings.py
+++
@@ -1,8 +0,0 @@
-"""Configuration module - NumberSpecificationParser for entity registry.
-
-ApplicationConfig has been removed. Use EntityRegistry for configuration.
-"""
-
-from .number_parser import NumberSpecificationParser
-
-__all__ = ["NumberSpecificationParser"]
